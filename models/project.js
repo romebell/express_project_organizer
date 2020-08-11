4e94e7f71c3d9fe29d56e0a81b8cd5cdaf0f82f6
blangwell <b.langwell@outlook.com>
2020-08-11 16:17:13 -0700

    [model] Create category model

--- models/project.js
+++ models/project.js
@@ -2,6 +2,7 @@
 module.exports = (sequelize, DataTypes) => {
   const project = sequelize.define('project', {
     name: DataTypes.STRING,
+    // validates if string is url
     githubLink: {
       type: DataTypes.TEXT,
       validate: {
