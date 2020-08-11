9782f410c948016fb221121ed8b78de5ba77d6ab
Martin Briceno <martin.briceno@live.com>
2020-08-11 19:17:34 -0400

    [model] create category model

--- dbTest.js
+++ dbTest.js
@@ -11,48 +11,57 @@ var async = require('async')
 // Create a project and use the helper function create<ModelName> to create a category
 // Requires categoriesProjects to exist, be migrated, and properly associated
 
-var cats = ['node', 'javascript', 'react', 'css', 'html']
+// var cats = ['node', 'javascript', 'react', 'css', 'html']
 
-db.project.create({
-  name: 'PROJECT TWO',
-  deployLink: 'http://github.com/brandiw',
-  githubLink: 'http://github.com/brandiw',
-  description: 'This was a game'
-}).then(function(project) {
-  // IMPROVED VERSION WITH ASYNC
-  // async.forEach(arrayToIterate, iteratorFunctionToRunOnEachItem(item, callback), functionToRunWhenAllComplete)
-  async.forEach(cats, (cat, done) => {
-    db.category.findOrCreate({
-      where: { name: cat }
-    })
-    .spread((category, wasCreated) => {
-      project.addCategory(category)
-      .then(() => {
-        // res.redirect, or whatevs
-        console.log('done adding', cat)
-        done()
-      })
-    })
-  }, () => {
-    console.log('EVERYTHING is done. Now redirect or something')
-  })
-
-
+// db.project.create({
+//   name: 'PROJECT TWO',
+//   deployLink: 'http://github.com/brandiw',
+//   githubLink: 'http://github.com/brandiw',
+//   description: 'This was a game'
+// }).then(function(project) {
+//   // IMPROVED VERSION WITH ASYNC
+//   // async.forEach(arrayToIterate, iteratorFunctionToRunOnEachItem(item, callback), functionToRunWhenAllComplete)
+//   async.forEach(cats, (cat, done) => {
+//     db.category.findOrCreate({
+//       where: { name: cat }
+//     })
+//     .spread((category, wasCreated) => {
+//       project.addCategory(category)
+//       .then(() => {
+//         // res.redirect, or whatevs
+//         console.log('done adding', cat)
+//         done()
+//       })
+//     })
+//   }, () => {
+//     console.log('EVERYTHING is done. Now redirect or something')
+//   })
 
 
 
-  // TIMING DOESNOT WORK
-  // cats.forEach((cat) => {
-  //   db.category.findOrCreate({
-  //     where: { name: cat }
-  //   })
-  //   .spread((category, wasCreated) => {
-  //     project.addCategory(category)
-  //     .then(() => {
-  //       // res.redirect, or whatevs
-  //       console.log('done adding', cat)
-  //     })
-  //   })
-  // })
-  // console.log('redirect or something')
+
+
+//   // TIMING DOESNOT WORK
+//   // cats.forEach((cat) => {
+//   //   db.category.findOrCreate({
+//   //     where: { name: cat }
+//   //   })
+//   //   .spread((category, wasCreated) => {
+//   //     project.addCategory(category)
+//   //     .then(() => {
+//   //       // res.redirect, or whatevs
+//   //       console.log('done adding', cat)
+//   //     })
+//   //   })
+//   // })
+//   // console.log('redirect or something')
+// })
+
+db.category.create({
+  name: 'node'
+}).then((category, result) => {
+  console.log(`this was created ${category.id}`);
+  console.log(category.get());
+}).catch(err => {
+  console.log(`error ${err}`);
 })
