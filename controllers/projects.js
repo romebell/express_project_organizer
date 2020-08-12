e237dcb5fb75056e1020862c7e113648ad70643c
Elaine <elaine@Felipes-iMac.local>
2020-08-11 23:45:52 -0500

    [projects] still puzzling over the project.addCategory doesn't seem to function the way it was described in class

--- controllers/projects.js
+++ controllers/projects.js
@@ -4,15 +4,31 @@ let router = express.Router()
 
 // POST /projects - create a new project
 router.post('/', (req, res) => {
+  
   db.project.create({
     name: req.body.name,
     githubLink: req.body.githubLink,
     deployLink: req.body.deployedLink,
-    description: req.body.description
+    description: req.body.description,
   })
-  .then((project) => {
-    res.redirect('/')
+
+  .then(project => {
+  db.category.findOrCreate({
+    where: { name: req.body.category }
   })
+
+    .then(category => {
+      project.addCategory(category)
+      res.redirect('/')
+      })
+
+      .catch(err => {
+        console.log(err)
+      })
+  .catch(err =>{
+    console.log(err)
+  })
+})
   .catch((error) => {
     res.status(400).render('main/404')
   })
