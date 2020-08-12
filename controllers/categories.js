7f7bf85ec571b122bde655855b1bf0e154128448
imjchiang <jchiang296@gmail.com>
2020-08-11 23:27:28 -0700

    [EJS] show works now

--- controllers/categories.js
+++ controllers/categories.js
@@ -3,15 +3,34 @@ let db = require("../models");
 let router = express.Router();
 
 router.get('/', (req, res) => {
-    db.category.findAll()
-    .then(category => 
+    db.category.findAll(
     {
-        res.render('categories/show', {category: category});
+        include: [db.project]
+    })
+    .then(categories => 
+    {
+        res.render('categories/show', {category: categories});
     })
     .catch(err => 
     {
         console.log(err);
     })
 })
+
+// router.get('/:id', (req, res) => {
+//     db.category.findOne({
+//         where: {
+//             id: req.params.id
+//         },
+//         include: [db.project]
+//     })
+//     .then(category => {
+//         console.log(category.projects)
+//         res.render('categories/show', {category: category })
+//     })
+//     .catch(error => {
+//         console.log('Error', error)
+//     })
+// })
 
 module.exports = router; 
