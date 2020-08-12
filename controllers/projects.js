a0bd6494334f2a3df93f7f5c2c8690ff7304e805
Martin Briceno <martin.briceno@live.com>
2020-08-12 12:02:19 -0400

    martin Briceno Pre class

--- controllers/projects.js
+++ controllers/projects.js
@@ -3,23 +3,22 @@ let db = require('../models')
 let router = express.Router()
 
 // POST /projects - create a new project
-router.post('/', async (req, res) => {
-  await db.project.create({
+router.post('/', (req, res) => {
+    db.project.create({
     name: req.body.name,
     githubLink: req.body.githubLink,
     deployLink: req.body.deployedLink,
     description: req.body.description
   })
-  .then(([project, created])=>{
-    console.log(created);
+  .then((project)=>{
+    console.log(project);
     db.category.findOrCreate({
       where : {
-        category: req.body.category
+        name: req.body.category
       }
     })
-    .then(([category, created])=>{
+    .then((category)=>{
       project.addCategory(category)
-      console.log(created)
       .then(newRelationship =>{
         console.log(newRelationship)
       })
@@ -58,5 +57,7 @@ router.get('/:id', (req, res) => {
     res.status(400).render('main/404')
   })
 })
+
+
 
 module.exports = router
