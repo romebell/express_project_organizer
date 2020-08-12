5e407f7103bff34fa44691de4dccb32b5f70b0b3
General Assembly <generalassembly@Davids-MacBook-Pro.local>
2020-08-11 22:11:17 -0700

    [views] Add categories/views, attempt ejs page and routes

--- controllers/categories.js
+++ controllers/categories.js
@@ -2,4 +2,30 @@ let express = require('express')
 let db = require('../models')
 let router = express.Router()
 
+// GET /categories
+router.get('/', (req, res) => {
+    db.category.findAll()
+    .then((categories) => {
+      res.render('main/index', { categories: categories })
+    })
+    .catch((error) => {
+      console.log('Error in GET /', error)
+      res.status(400).render('main/404')
+    })
+  })
+
+// GET /categories/:id
+router.get('/:id', (req, res) => {
+    db.category.findOne({
+      where: { id: req.params.id }
+    })
+      .then((category) => {
+        if (!category) throw Error()
+        res.render('categories/show', { category: category })
+      })
+      .catch((error) => {
+        res.status(400).render('main/404')
+      })
+  })
+
 module.exports = router
